dfc75a2f08abda433aca49ce108d529e23152898
asyaasha <asya.asha@gmail.com>
2017-07-23 16:20:23 -0400

    add factory girl

--- app/models/gram.rb
+++ app/models/gram.rb
@@ -1,4 +1,6 @@
 class Gram < ApplicationRecord
-  validates :message, length: { minimum: 5}
+  # validates :message, length: { minimum: 5}
   validates :message, presence: true
+
+  belongs_to :user
 end
