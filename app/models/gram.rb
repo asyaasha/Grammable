b611bbcba6561b6f75f4fa0d2715a70d61c8848a
asyaasha <asya.asha@gmail.com>
2017-07-29 03:47:10 -0400

    add style

--- app/models/gram.rb
+++ app/models/gram.rb
@@ -3,6 +3,7 @@ class Gram < ApplicationRecord
   validates :message, length: { minimum: 5}
   validates :message, presence: true
   validates :picture, presence: true
+  has_many :comments
 
   belongs_to :user
 end
